35e26ece67cb0838336416f0413c295b085f4121
rkhoury11 <rkhoury11@icloud.com>
2024-04-23 19:12:54 -0400

    finished portfolio

--- js/script.js
+++ js/script.js
@@ -1,3 +1,8 @@
-document.querySelector("img").addEventListener("click", function(){
-    
+document.getElementById("email").addEventListener("click", function(){
+    const myEmail = "rkhoury11@icloud.com";
+    const subject = "Contacting from personal website";
+
+    const mailToLink = `mailto:${myEmail}?subject=${encodeURIComponent(subject)}`;
+
+    window.location.href = mailToLink;
 });
